cfb1d140b90296bd0fe5885008b3ba83e28c4131
佐々木 <66350042+FOV90@users.noreply.github.com>
2020-09-17 11:35:28 +0900

    キャラクター定数定義

--- yoketoruvs20/Form1.cs
+++ yoketoruvs20/Form1.cs
@@ -15,6 +15,21 @@ namespace yoketoruvs20
     {
         const bool isDebug = true;
 
+        const int PlayerMax = 1;
+        const int EnemyMax = 10;
+        const int ItemMax = 10;
+        const int ChrMax = PlayerMax + EnemyMax + ItemMax;
+        Label[] chrs = new Label[ChrMax];
+        const int PlayerIndex = 0;
+        const int EnemyIndex = PlayerIndex + PlayerMax;
+        const int ItemiIdex = EnemyIndex + EnemyMax;
+
+        const string PlayerText = "(*'▽')";
+        const string EnemyText = "🔹";
+        const string ItemText = "★";
+
+        static Random rand = new Random();
+
         enum State
         {
             None = -1,  // 無効
